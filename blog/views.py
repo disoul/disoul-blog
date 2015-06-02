8733c8a02e263d897fdc979bc72fda7c5b92bc1f
disoul <starky_ds@sina.cn>
2015-06-02 12:10:46 +0800

    add tag block

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
+from blog.models import ArticleTags
 
 def home(request):
     homepage = get_template('home.html')
-    html = homepage.render(Context())
-    return HttpResponse(html)
+    context = Context({
+              'tags':ArticleTags.objects.all()
+        })
+    return HttpResponse(homepage.render(context))
