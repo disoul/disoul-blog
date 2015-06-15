35474db9ad3b767fdfc9e0b8e8b42e7dd5d5d8f0
disoul <starky_ds@sina.cn>
2015-06-15 10:02:07 +0800

    add markdowntag

--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,14 @@ from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
 from blog.models import ArticleTags,Article
-import markdown2
 
 def home(request):
     homepage = get_template('home.html')
     tag_objs = ArticleTags.objects.all()
     article_objs = Article.objects.all()
-    for article_obj in article_objs:
-        article_obj.content = MarkdownParse(article_obj.content)
-        article_obj.tag = [tag for tag in  article_obj.tag.all()]
     context = Context({
               'tags':tag_objs,
               'articles':article_objs,
         })
     return HttpResponse(homepage.render(context))
 
-def MarkdownParse(content):
-    return markdown2.markdown(content,extras=['fenced-code-blocks'])
--- disoul_blog/static/js/index.js
+++
@@ -1,4 +0,0 @@
-function linkToHome(){
-    window.location.href="file:///home/disoul/Works/disoul_blog/disoul_blog/templates/base.html";
-
-}
