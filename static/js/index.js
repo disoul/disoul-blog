390ec956de8607c32f62ea142c8d62dbac0fce20
disoul <starky_ds@sina.cn>
2015-07-14 00:21:45 +0800

    django+uwsgi+nginx

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+var leftflag = 1;
+
+$(document).ready(function(){
+		$(".left-list-control").click(function(){
+			  if (leftflag > 0){
+						$(".left-list").animate({width: '220px'});
+						$(".left-list-control i").removeClass('fa-angle-double-right');
+						$(".left-list-control i").addClass('fa-angle-double-left');
+						leftflag = -leftflag;
+			  }else{
+						$(".left-list").animate({width: '0px'});	
+						$(".left-list-control i").removeClass('fa-angle-double-left');
+						$(".left-list-control i").addClass('fa-angle-double-right');
+						leftflag = -leftflag;
+			 }
+		});
+		$("#github").click(function(){
+				window.location.href="http://github.com/disoul/disoul-blog";
+		});
+});
