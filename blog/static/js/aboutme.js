e7053cfee5b25a9fb314981ee2aa41feebbab588
disoul <starky_ds@sina.cn>
2015-07-17 15:39:00 +0800

    fix js

--- blog/static/js/aboutme.js
+++ blog/static/js/aboutme.js
@@ -1,3 +1,3 @@
-var days = Math.round((Date.now()-Date.UTC(2014,7,11))/1000/3600/24);
+var days = Math.round((Date.now()-Date.UTC(2014,6,11))/1000/3600/24);
 document.getElementById("lovedays").innerHTML=days.toString()+"<span>天</span>";
 
