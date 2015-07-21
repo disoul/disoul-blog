218ecf253055d3a37e6241f19fa6d279041e3464
disoul <starky_ds@sina.cn>
2015-07-21 20:37:13 +0800

    继续修改适配C

--- blog/static/js/index.js
+++ blog/static/js/index.js
@@ -3,12 +3,12 @@ var leftflag = 1;
 $(document).ready(function(){
 		$(".left-list-control").click(function(){
 			  if (leftflag > 0){
-						$(".left-list").animate({width: '220px'});
+						$(".left-list-mask").animate({width: '120px'});
 						$(".left-list-control i").removeClass('fa-angle-double-right');
 						$(".left-list-control i").addClass('fa-angle-double-left');
 						leftflag = -leftflag;
 			  }else{
-						$(".left-list").animate({width: '0px'});	
+						$(".left-list-mask").animate({width: '0px'});	
 						$(".left-list-control i").removeClass('fa-angle-double-left');
 						$(".left-list-control i").addClass('fa-angle-double-right');
 						leftflag = -leftflag;
--- blog/tasks.py
+++ blog/tasks.py
@@ -3,8 +3,8 @@ import shlex,subprocess
 
 @task
 def update_work():
-#    git = subprocess.Popen(shlex.split("git pull origin master"))
-#    git.wait()
+    git = subprocess.Popen(shlex.split("git pull origin master"))
+    git.wait()
     update = subprocess.Popen(shlex.split("/usr/disoul/works/disoul-blog/add_article.py"))
     update.wait()
     return 1
