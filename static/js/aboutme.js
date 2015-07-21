4cbb65a5b32732bae154925b2ed7d6cb367edee8
disoul <starky_ds@sina.cn>
2015-07-21 21:51:03 +0800

    ignore static files

--- static/js/aboutme.js
+++
@@ -1,3 +0,0 @@
-var days = Math.round((Date.now()-Date.UTC(2014,7,11))/1000/3600/24);
-document.getElementById("lovedays").innerHTML=days.toString()+"<span>天</span>";
-
--- static/js/base.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-		$("#name").click(function(){
-				window.location.href="/blog/";
-		});
-});
