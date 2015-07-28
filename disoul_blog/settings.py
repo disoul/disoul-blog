b64a23b0a322c722c66338a7fbd19328cfb5254f
disoul <starky_ds@sina.cn>
2015-07-28 22:50:15 +0800

    DEBUG=false

--- disoul_blog/settings.py
+++ disoul_blog/settings.py
@@ -27,7 +27,7 @@ BROKER_URL = 'redis://localhost:6379/0'
 SECRET_KEY = '!yk@1+^*z%du%6hpzni!a%(%56pn-02slr9#8cu&n&w-7=_&c)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',]
 
@@ -105,8 +105,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/usr/disoul/works/disoul-blog/static'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ('/usr/disoul/works/disoul-blog/blog/static',
+STATICFILES_DIRS = ('/usr/disoul/works/disoul-blog/static',
                    ) 
 
--- disoul_blog/urls.py
+++ disoul_blog/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings 
 
 admin.autodiscover();
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.STATIC_ROOT,}),
 ]
