42488add64e84c286598eb859feeb893261f840b
disoul <starky_ds@sina.cn>
2015-07-28 21:52:02 +0800

    sass Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,91 +5,64 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    bower: {
-      install: {
-	options: {
-	  "targetDir": "/usr/disoul/works/disoul-blog/blog/static/lib",
-          "layout": "byType",
-          "install": true,
-          "verbose": false,
-          "cleanTargetDir": false
-	}
-      }
-    },
+    meta: {
+	  scssPath: './blog/static/css/scss/',
+	  cssPath: './blog/static/css/'
+	},
+
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
+    // Bower
+    bower: {
+      install: {
+		options: {
+		  "targetDir": "./blog/static/lib",
+          "layout": "byType",
+          "install": true,
+          "verbose": false,
+          "cleanTargetDir": false
+		}
       }
     },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
+
+	// Sass
+    sass:{
+	  dist:{
+	    options:{
+		  style: "compressed",
+		},
+		files: {
+		  '<%= meta.cssPath %>index.css': '<%= meta.scssPath %>index.scss',
+		  '<%= meta.cssPath %>tag.css': '<%= meta.scssPath %>tag.scss',
+		  '<%= meta.cssPath %>aboutme.css': '<%= meta.scssPath %>aboutme.scss',
+		  '<%= meta.cssPath %>article.css': '<%= meta.scssPath %>article.scss'
+		}
+	  }
+	},
+
+	// Watch
     watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      }
+	  script: {
+		files: [
+		  '<%= meta.scssPath%>/**/*.scss'
+		],
+		tasks: ['sass'],
+		options: {
+		},
+	  }
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-bower-task');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['bower','jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['sass']);
 
 };
