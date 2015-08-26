5810a6bd8bef879016e3d3a33c3a2e8e189ef7bf
disoul <starky_ds@sina.cn>
2015-08-26 20:22:45 +0800

    gulp-compass

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    compass = require('gulp-compass'),
+	path = require('path');
+
+gulp.task('compass',function(){
+	gulp.src('./blog/static/sass/index.scss')
+	.pipe(compass({
+		project: path.join(__dirname,'./blog/static'),
+		css: 'css',
+		sass: 'sass',
+		image: 'image'
+	}));
+});
+
+gulp.task('sass:watch',function(){
+	gulp.watch('./blog/static/sass/**/*.scss',['compass']);
+});
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
